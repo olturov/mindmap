5bfc5af3132ede6d21dab22728f8ae47841bfd8b
Shell <shllvshll@gmail.com>
2023-06-13 08:06:32 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { ChildrenOutletContexts } from '@angular/router';
+import { slideInAnimation } from './animations';
+
+
+@Injectable()
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './css/style.css'],
+  animations: [
+    slideInAnimation
+  ]
 })
 export class AppComponent {
-  title = 'mindmap';
+  constructor(private contexts: ChildrenOutletContexts) {}
+
+getRouteAnimationData() {
+  return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
 }
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HipageComponent } from './hipage/hipage.component';
+import { WhatisComponent } from './whatis/whatis.component';
+import { CardsComponent } from './cards/cards.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HipageComponent,
+    WhatisComponent,
+    CardsComponent,
+    AboutmeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
